7228b9e24bc074112eebb5139f50d4ccf7967f6c
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-01 20:50:09 -0500

    Changed the placeholder function's name to .localtestfun

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,17 +1,17 @@
 .onLoad <- function(libname, pkgname){
     # Load the functions stored...
     functionsPath <- file.path(find.package("local"), "localfunctions")
-    #functionsPath <- getwd()
     
     loadfunction <- function(filename){
         file <- file.path(functionsPath, filename)
         fun <- readRDS(file)
-        functionname <- gsub(".rds", "", filename)
+        functionname <- gsub(".rds$", "", filename)
         #TODO: check to make sure it's a valid file and what not
         eval(parse(text = paste0("assign(",shQuote(functionname),", fun, envir = globalenv())")))
     }
     
-    functions <- grep("*.rds", dir(functionsPath), value = TRUE)
+    functions <- grep("*.rds", dir(functionsPath, all = TRUE), value = TRUE)
+    functions <- functions[functions != ".localtestfun.rds"]
     lapply(functions, loadfunction)
     return()
 }
