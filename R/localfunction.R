51e2f36c2ad8e85411e28c9cdc71baa7637952a8
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-01 19:50:26 -0500

    First Commit.

---
+++ R/localfunction.R
@@ -0,0 +1,17 @@
+#' Store a new function into the local database
+#' 
+#' Store a new function into the local database
+#' 
+#' @param func The function you want to store
+#' @export
+#' @examples
+#' \dontrun{
+#' myfun <- function(x){x + 2}
+#' localfunction(myfun)
+#' }
+localfunction <- function(func){
+     functionsPath <- file.path(find.package("local"), "localfunctions")
+     functionName <- as.character(substitute(func))
+     saveRDS(func, file = file.path(functionsPath, paste0(functionName, ".rds")))
+     #functionName
+}
---
+++ R/onload.R
@@ -0,0 +1,9 @@
+.onLoad <- function(libname, pkgname){
+    # Load the functions stored...
+    #functionsPath <- file.path(find.package("local"), "localfunctions")
+    functionsPath <- getwd()
+    
+    functions <- grep("*.rds", dir(functionsPath), value = TRUE)
+    lapply(functions, loadfunction)
+    
+}
