15eecd1acad7df75c93279e5747e1321e65699b8
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-01 20:58:38 -0500

    Added documentation to localfunction.

--- R/localfunction.R
+++ R/localfunction.R
@@ -1,8 +1,12 @@
 #' Store a new function into the local database
 #' 
-#' Store a new function into the local database
+#' Store a new function into the local database.  When you
+#' store a function then everytime you load local your
+#' function will be reloaded into the global environment.  This
+#' is a convenient alternative to saving your function in a script
+#' if you're working on the fly.
 #' 
-#' @param func The function you want to store
+#' @param func The function you want to store.
 #' @export
 #' @examples
 #' \dontrun{
@@ -13,5 +17,4 @@ localfunction <- function(func){
      functionsPath <- file.path(find.package("local"), "localfunctions")
      functionName <- as.character(substitute(func))
      saveRDS(func, file = file.path(functionsPath, paste0(functionName, ".rds")))
-     #functionName
 }
