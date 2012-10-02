ab7d6cc929ca4f431dcac277cc885e84934c47f3
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-01 22:33:43 -0500

    Moved functionality in onload to its own function

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,21 +1,3 @@
 .onLoad <- function(libname, pkgname){
-    # Load the functions stored...
-    databasePath <- file.path(find.package("local"), "localdatabase")
-    
-    loadItem <- function(filename){
-        file <- file.path(databasePath, filename)
-        item <- readRDS(file)
-        itemName <- gsub(".rds$", "", filename)
-        
-        cmd <- paste0("assign(",
-                      shQuote(itemName),
-                      ", item, envir = globalenv())")
-        #TODO: check to make sure it's a valid file and what not
-        eval(parse(text = cmd))
-    }
-    
-    items <- grep("*.rds", dir(databasePath, all.files = TRUE), value = TRUE)
-    items <- items[items != "localplaceholder.rds"]
-    lapply(items, loadItem)
-    return()
+    loadAll()
 }
